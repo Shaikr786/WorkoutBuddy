d537f34c3d8af7536ae4b58d03a717501daf629e
Reshma Shaik <reshma786jrrs@gmail.com>
2025-02-21 19:26:40 +0530

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -23,6 +23,18 @@ app.use(cors({
     credentials: true
   }));
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://workout-buddy-website.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204); // Handle preflight requests
+    }
+    next();
+});
+
+
 app.use('/api/workouts' ,workoutRoutes)
 app.use('/api/user' ,UserRoutes)
 mongoose.connect(process.env.URI)
